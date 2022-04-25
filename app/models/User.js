6bf0b27bff119260274ec961ac5d049048a7f62b
hisahito <hisahito706@gmail.com>
2022-04-24 21:33:41 -0500

    4to commit, adding second test and adding getters to the class

--- app/models/User.js
+++ app/models/User.js
@@ -6,8 +6,20 @@ class twitter {
         this.user = user;
         this.name = name;
         this.bio = bio;
-        this.dateCreated = dateCreated;
-        this.lastUpdated = lastUpdated;
+        this.dateCreated = new Date();
+        this.lastUpdated = new Date();
+    }
+    get getUsername() {
+        return this.user
+    }
+    get getBio() {
+        return this.bio
+    }
+    get getDateCreated() {
+        return this.dateCreated
+    }
+    get getLastUpdated() {
+        return this.lastUpdated
     }
 
 
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -7,12 +7,19 @@ const User = require('./../../app/models/User');
 describe("Unit Tests for User class", () => {
 //invocando el codigo a probar
     test("create an User object", () => {
-        const user = new User(1, "hisahito", "isai", "bio", "dateCreated", "lastUpdated");
+        const user = new User(1, "hisahito", "isai", "bio");
         expect(user.id).toBe(1);
         expect(user.user).toBe("hisahito");
         expect(user.name).toBe("isai");
         expect(user.bio).toBe("bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
+    })
+    test("adding getters", () => {
+        const user = new User(1, "hisahito", "isai", "bio");
+        expect(user.user).toBe("hisahito");
+        expect(user.bio).toBe("bio");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
     })
 })
